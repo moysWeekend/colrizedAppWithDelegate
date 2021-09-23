7abd5ef8139d82d41374f38f5a067b2d73cc5e28
moysWeekend <a.b.burtsev@gmail.com>
2021-09-23 17:04:40 +0300

    added logic without work with textFields

--- colrizedAppWithDelegate/SettingsViewController.swift
+++ colrizedAppWithDelegate/SettingsViewController.swift
@@ -36,6 +36,9 @@ class SettingsViewController: UIViewController {
         colorView.backgroundColor = viewColor
         
         setSliders()
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+        setValue(for: redValueTextField, greenValueTexField, blueValueTextField)
+        
         
     }
     
@@ -45,38 +48,63 @@ class SettingsViewController: UIViewController {
         
         switch sender {
         case redSlider:
-            redValueLabel.text = String(format: "%.2f", redSlider.value)
-            redValueTextField.text = String(format: "%.2f", redSlider.value)
+            setValue(for: redValueLabel)
+            setValue(for: redValueTextField)
         case greenSlider:
-            greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-            greenValueTexField.text = String(format: "%.2f", greenSlider.value)
-        case blueSlider:
-            blueValueLabel.text = String(format: "%.2f", blueSlider.value)
-            blueValueTextField.text = String(format: "%.2f", blueSlider.value)
+            setValue(for: greenValueLabel)
+            setValue(for: greenValueTexField)
         default:
-            break
+            setValue(for: blueValueLabel)
+            setValue(for: blueValueTextField)
         }
         mixingColors()
     }
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        delegate.setColor(colorView.backgroundColor ?? .white)
+        dismiss(animated: true)
+    }
 }
 
-// MARK: - Private Methods
-extension SettingsViewController {
-    
-    private func mixingColors() {
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value), alpha: 1
-        )
-    }
-    private func setSliders() {
-        let ciColor = CIColor(color: viewColor)
+    // MARK: - Private Methods
+    extension SettingsViewController {
         
-        redSlider.value = Float(ciColor.red)
-        greenSlider.value = Float(ciColor.green)
-        blueSlider.value = Float(ciColor.blue)
+        private func mixingColors() {
+            colorView.backgroundColor = UIColor(
+                red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value), alpha: 1
+            )
+        }
+        private func setSliders() {
+            let ciColor = CIColor(color: viewColor)
+            
+            redSlider.value = Float(ciColor.red)
+            greenSlider.value = Float(ciColor.green)
+            blueSlider.value = Float(ciColor.blue)
+        }
+        
+        private func setValue(for labels: UILabel...) {
+            labels.forEach { label in
+                switch label {
+                case redValueLabel: label.text = string(from: redSlider)
+                case greenValueLabel: label.text = string(from: greenSlider)
+                default: label.text = string(from: blueSlider)
+                }
+            }
+        }
+        
+        private func setValue(for textFields: UITextField...) {
+            textFields.forEach { textField in
+                switch textField {
+                case redValueTextField: textField.text = string(from: redSlider)
+                case greenValueTexField: textField.text = string(from: greenSlider)
+                default: textField.text = string(from: blueSlider)
+                }
+            }
+        }
+        
+        private func string(from slider: UISlider) -> String {
+            String(format: "%.2f", slider.value)
+        }
     }
-}
-
 
